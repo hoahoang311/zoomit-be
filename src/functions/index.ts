ac9bc781dde3c84e9637a7ca6892e66ad8dd4449
Ken Hoang <hqhoang311@gmail.com>
2024-09-29 19:10:48 -0400

    hello function

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -2,7 +2,5 @@ import { APIGatewayProxyEvent } from 'aws-lambda';
 import { createResponse } from '../util/common';
 
 export const handler = async (e: APIGatewayProxyEvent) => {
-    console.log('testing network', e);
-
-    return createResponse(200, { success: true });
+    return createResponse(200, { success: true, message: 'Hello World' });
 };
