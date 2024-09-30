0f94f0240a4f0059a30ecca6b63d422fc9390587
Ken Hoang <hqhoang311@gmail.com>
2024-09-30 01:14:01 -0400

    add fetch

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -2,5 +2,9 @@ import { APIGatewayProxyEvent } from 'aws-lambda';
 import { createResponse } from '../util/common';
 
 export const handler = async (e: APIGatewayProxyEvent) => {
-    return createResponse(200, { success: true, message: 'Hello World' });
+    const res = await fetch('https://66hunpjaub.execute-api.ca-central-1.amazonaws.com/Prod');
+
+    console.log(res);
+
+    return createResponse(200, { success: true, message: res });
 };
