983d2e67bd9c36f32f11cf238bdc0fa022b01611
Ken Hoang <hqhoang311@gmail.com>
2024-09-30 03:29:01 -0400

    add log

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -6,6 +6,7 @@ export const handler = async (e: APIGatewayProxyEvent) => {
         const res = await fetch('https://66hunpjaub.execute-api.ca-central-1.amazonaws.com/Prod');
 
         console.log(res);
+        console.log(e);
 
         return createResponse(200, { success: true, message: res });
     } catch (err: any) {
