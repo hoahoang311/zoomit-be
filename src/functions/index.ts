a8cd552fc8eebcfe7819d2d4931f6f46b38f8af9
Ken Hoang <hqhoang311@gmail.com>
2024-09-30 03:41:31 -0400

    update function

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -3,7 +3,12 @@ import { createResponse } from '../util/common';
 
 export const handler = async (e: APIGatewayProxyEvent) => {
     try {
-        const res = await fetch('https://66hunpjaub.execute-api.ca-central-1.amazonaws.com/Prod');
+        const res = await fetch('https://66hunpjaub.execute-api.ca-central-1.amazonaws.com/Prod', {
+            method: 'GET',
+            headers: {
+                Accept: 'application/json',
+            },
+        });
 
         console.log(res);
         console.log(e);
